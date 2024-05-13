da45809855a97db4487c3a2257d4ca534944ba6c
Marcelo Martins <marcelo.martins@ms.senac.br>
2024-05-13 14:01:21 -0400

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        // api/projects/599
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) 
+        {
+            //return NotFound();
+            return Ok();
+
+        }
+    }
+}
